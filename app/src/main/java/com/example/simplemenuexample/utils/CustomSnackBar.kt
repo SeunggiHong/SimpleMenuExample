0d90b03342cb38d2b879807610aae467958576d3
seunggiHong <tmdrl1026@naver.com>
2021-08-24 23:22:27 +0900

    Add KoinComponent for DI into class

--- app/src/main/java/com/example/simplemenuexample/utils/CustomSnackBar.kt
+++ app/src/main/java/com/example/simplemenuexample/utils/CustomSnackBar.kt
@@ -1,27 +1,29 @@
 package com.example.simplemenuexample.utils
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.simplemenuexample.R
 import com.example.simplemenuexample.databinding.ItemSnackbarBinding
+import com.example.simplemenuexample.viewmodels.TestViewModel
 import com.google.android.material.snackbar.Snackbar
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-
-class CustomSnackBar(view: View, private val type: Int, private val message: String) {
+class CustomSnackBar(view: View, private val type: Int, private val message: String): KoinComponent {
 
     companion object {
         fun make(view: View, type: Int, message: String) = CustomSnackBar(view, type, message)
     }
 
     private val context = view.context
-    private val snackbar = Snackbar.make(view, "", 3000)
+    private val snackbar = Snackbar.make(view, "", 4000)
     private val snackbarLayout = snackbar.view as Snackbar.SnackbarLayout
 
     private val inflater = LayoutInflater.from(context)
     private val binding: ItemSnackbarBinding = DataBindingUtil.inflate(inflater, R.layout.item_snackbar, null, false)
+    private val testViewModel: TestViewModel by inject()
 
     init {
         initView()
@@ -53,6 +55,11 @@ class CustomSnackBar(view: View, private val type: Int, private val message: Str
         }
 
         binding.btnSnack.setOnClickListener {
+            when(type) {
+                Constants.SNACK_RETRY -> {
+                    testViewModel.fetchTestUsers()
+                }
+            }
 
         }
     }
