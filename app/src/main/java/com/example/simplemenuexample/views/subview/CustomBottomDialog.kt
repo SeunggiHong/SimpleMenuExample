6aee4a99293954f3921523d4e43cc3a8e7b0273e
seunggiHong <tmdrl1026@naver.com>
2021-08-26 22:42:30 +0900

    Add onViewCreated and onClick for BottomSheetDialog

--- app/src/main/java/com/example/simplemenuexample/views/subview/CustomBottomDialog.kt
+++ app/src/main/java/com/example/simplemenuexample/views/subview/CustomBottomDialog.kt
@@ -1,16 +1,25 @@
 package com.example.simplemenuexample.views.subview
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.DrawableContainer
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.simplemenuexample.R
+import com.example.simplemenuexample.databinding.DialogBottomSheetBinding
+import com.example.simplemenuexample.utils.Constants
+import com.example.simplemenuexample.utils.Constants.TAG
+import com.example.simplemenuexample.views.SnackBarActivity
+import com.example.simplemenuexample.views.UserActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class CustomBottomDialog: BottomSheetDialogFragment() {
+class CustomBottomDialog: BottomSheetDialogFragment(), View.OnClickListener {
+    private var _binding: DialogBottomSheetBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +32,53 @@ class CustomBottomDialog: BottomSheetDialogFragment() {
             savedInstanceState: Bundle?
     ): View? {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        return inflater.inflate(R.layout.item_bottom_sheet, container)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.dialog_bottom_sheet, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvRecycler.setOnClickListener(this)
+        binding.tvSnack.setOnClickListener(this)
+        binding.tvPopup.setOnClickListener(this)
+        binding.tvViewpager.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            binding.tvRecycler.id -> {
+                val intent = Intent(activity, UserActivity::class.java)
+                startActivity(intent)
+                this.dismiss()
+            }
+            binding.tvSnack.id -> {
+                val intent = Intent(activity, SnackBarActivity::class.java)
+                startActivity(intent)
+                this.dismiss()
+            }
+            binding.tvPopup.id -> {
+                showUserAddDialog()
+                this.dismiss()
+            }
+            binding.tvViewpager.id -> {
+
+            }
+        }
+    }
+
+    private fun showUserAddDialog() {
+        childFragmentManager.beginTransaction().replace(R.id.fragment_container, UserAddDialogFragment()).commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "UserAddDialogFragment - onDestroyView() called")
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "CustomBottomDialog - onDestroy() called")
+    }
 
 }
