de71a31576b27ccad3f3a47c964cf8d0ba52be8b
seunggiHong <tmdrl1026@naver.com>
2021-08-29 20:14:18 +0900

    Add Page index for viewpager2

--- app/src/main/java/com/example/simplemenuexample/utils/Constants.kt
+++ app/src/main/java/com/example/simplemenuexample/utils/Constants.kt
@@ -13,6 +13,12 @@ object Menu {
     const val MENU_SNACKBAR_VIEW = 1
     const val MENU_POPUP_DIALOG_VIEW = 2
     const val MENU_BOTTOM_SHEET_DIALOG = 3
+    const val MENU_VIEW_PAGER = 4
+}
+
+object Page {
+    const val USER_PAGE_INDEX = 0
+    const val DETAIL_PAGE_INDEX = 1
 }
 
 enum class Status {
